0edcbb3cb7633ca9d677abe383af81b8b391e541
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-11-20 22:05:19 +0200

    Implement `jv-dependency-injection` project with models, services, custom annotations, dependency injection, Maven setup, and CI integration

---
+++ jv-dependency-injection/src/main/java/mate.academy/lib/Injector.java
@@ -0,0 +1,13 @@
+package mate.academy.lib;
+
+public class Injector {
+    private static final Injector injector = new Injector();
+
+    public static Injector getInjector() {
+        return injector;
+    }
+
+    public Object getInstance(Class<?> interfaceClazz) {
+        return null;
+    }
+}
